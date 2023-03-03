8985df82bd13621ff140d1be36417f261965cbf4
cuijingrui <382690498@qq.com>
2023-03-03 14:29:54 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Yyk\Eqbao;
+class Test
+{
+    public function show()
+    {
+        echo "this is show \n";
+    }
+}
---
+++ test/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+require '../vendor/autoload.php';
+spl_autoload_register(function ($class_name) {
+    echo $class_name . "\n";
+    require_once $class_name . '.php';
+});
+
+
+use Yyk\Eqbao\Test;
+
+
+$i = new Test();
+$i->show();
